16b5f800a6ee6c2c43841bb09540a90ed66253d4
Samuel <up210713@alumnos.upa.edu.mx>
2023-03-03 20:10:39 -0600

    Update1.3

--- U2/03_infix2postfix.py
+++ U2/03_infix2postfix.py
@@ -5,20 +5,31 @@ Pila = []
 i = 0 
 Q.insert(0, '(')
 Q.append(')')
-
+def prioridad(a):
+    if a in ['(', ')']:
+        return 0
+    elif a == '^':
+        return 3
+    elif a in ['*', '/']:
+       return 2
+    elif a in ['+', '-']:
+        return 1
+    
 for i in range (len(Q)):
     if Q[i] == '(':
         
         Pila.append(Q[i])
         
     elif Q[i] in ['+', '-', '*','/', '^']:
+        if prioridad(Q[i]) <= prioridad(Pila[len(Pila)-1]):
+            P.append(Pila.pop())
         Pila.append(Q[i])
     elif Q[i] == ')':
-        while i in range(len(P)):
-            P = Pila.pop()
-            if P[i] == '(':
-                Pila.pop()
-                break
+        j = len(Pila)-1
+        while Pila[j] != '(':
+            P.append(Pila.pop())
+            j-=1
+        Pila.pop()
     else:
         P.append(Q[i])
 print(P)
--- U3/Pilas2.py
+++ U3/Pilas2.py
@@ -6,6 +6,9 @@ class Nodo:
     def getData(self):
         return self.data
 
+#LIFO = Last Input First Output
+#UEPS = Ultimo en Entrar Ultimo en Salir
+
 class Stack:
     def __init__(self):
         self.head = None
@@ -25,3 +28,40 @@ class Stack:
         self.head = newNode
         self.size +=1
         ##TAREA: agregay a Jasus, Maria y Jose y hacer un pop de Jose
+
+    def pop(self):
+        dato = None
+        if not self.isEmtpy():
+            self.size -= 1
+            dato = self.head.data
+            self.head = self.head.next
+        return dato
+    
+    def peak(self):
+        dato = None
+        if not self.isEmtpy(): 
+            dato = self.head.data
+        return dato
+
+    def show (self):
+        cadena = ""
+        while self.head != None:
+            cadena += self.head.data + '-->'
+            self.head = self.head.next
+        return cadena
+
+pila = Stack()
+print(pila.peak())
+pila.push("Jesus")
+pila.push("Maria")
+pila.push("Jose")
+
+print("Sacar --------->")
+
+print(pila.pop())
+print(pila.peak())
+
+print('Metodo show')
+
+print(pila.show())
+
